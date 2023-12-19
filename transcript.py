efa6161b744ff3785730c596f876664d40b536c5
q <q>
2023-12-19 10:49:44 +0000

    python-mpv attempt

--- transcript.py
+++ transcript.py
@@ -1,6 +1,8 @@
 import mpv
-player = mpv.MPV(input_default_bindings=True, input_vo_keyboard=True, osc=True, ytdl=True)
-player.play('https://youtu.be/DOmdB7D-pUU')
+# plugins=['file-browser','uosc']   
+# player = mpv.MPV(input_default_bindings=True, input_vo_keyboard=True, osc=True, ytdl=True)
+player = mpv.MPV(input_default_bindings=True, input_vo_keyboard=True, osc=True)
+player['scripts'] = [r'C:\Programs\mpv\portable_config\scripts\file-browser.lua']
+player.play(r'D:\Downloads\1_delete\filian.mp4')
 player.wait_for_playback()
-
 # mp.get_property("path")
