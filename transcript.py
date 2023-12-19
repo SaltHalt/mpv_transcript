06922b41ce294f3e3e6e975b6869406d244c1b16
q <q>
2023-12-19 11:37:10 +0000

    Working base example

--- transcript.py
+++ transcript.py
@@ -1,8 +1,8 @@
-import mpv
-# plugins=['file-browser','uosc']   
-# player = mpv.MPV(input_default_bindings=True, input_vo_keyboard=True, osc=True, ytdl=True)
-player = mpv.MPV(input_default_bindings=True, input_vo_keyboard=True, osc=True)
-player['scripts'] = [r'C:\Programs\mpv\portable_config\scripts\file-browser.lua']
-player.play(r'D:\Downloads\1_delete\filian.mp4')
-player.wait_for_playback()
-# mp.get_property("path")
+from python_mpv_jsonipc import MPV
+mpv = MPV(start_mpv=False, ipc_socket=r"\\pipe\mpv_transcript_socket")
+@mpv.on_key_press("g")
+def space_handler():
+    print("Test")
+
+#mpv --input-ipc-server=\\pipe\mpv_transcript_socket filian.mp4
+#[System.IO.Directory]::GetFiles("\\.\\pipe\\") | findstr "mpv"
